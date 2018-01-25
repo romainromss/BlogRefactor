3c9fc95f921819cb98f4da4d37507312170eaa62
Unknown <romain.romss@gmail.com>
2018-01-25 18:30:24 +0100

    Add $_FILES

--- src/GetField.php
+++ src/GetField.php
@@ -3,13 +3,30 @@ namespace Romss;
 
 trait GetField
 {
+    /**
+     * @param array $field
+     * @return null
+     */
     protected function getField($field)
     {
         return $_POST[$field] ?? null;
     }
 
+    /**
+     * @param array $session
+     * @return null
+     */
     protected function  getSession($session)
     {
       return $_SESSION[$session] ?? null;
+    }
+
+    /**
+     * @param array $files
+     * @return null
+     */
+    protected function getFiles($files)
+    {
+        return $_FILES[$files] ?? null;
     }
 }
